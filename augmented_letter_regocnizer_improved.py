c55fdcd22fb78b16183372b092c02b3838305b16
JonasBachmeier-UNI <Jonas.Bachmeier@stud.uni-regensburg.de>
2024-07-14 16:57:26 +0200

    improve HandDetector and command menu

--- augmented_letter_regocnizer_improved.py
+++ augmented_letter_regocnizer_improved.py
@@ -1,6 +1,6 @@
 from threading import Thread
 from PyQt6.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QMainWindow
-from PyQt6.QtGui import QPainter, QPen
+from PyQt6.QtGui import QPainter, QPen, QFont
 from PyQt6.QtCore import Qt, QPointF, QThread, pyqtSignal
 import mediapipe as mp
 import cv2
@@ -42,6 +42,9 @@ class HandDetector(QThread):
     def __init__(self):
         super().__init__()
         self.options = HandLandmarkerOptions(
+            num_hands=1,
+            min_hand_detection_confidence=0.5,
+            min_tracking_confidence=0.5,
             base_options=BaseOptions(model_asset_path=model_path),
             running_mode=VisionRunningMode.LIVE_STREAM,
             result_callback=self.handle_result)
@@ -50,10 +53,6 @@ class HandDetector(QThread):
         self.gesture_being_drawn = False
         self.time_since_last_detection = np.inf
         self.line_points = []  # To store points for gesture recognition
-
-        # Init Dollar Recognizer
-        self.recognizer = DollarRecognizer()
-        self.recognizer.load_unistrokes_from_xml()
 
     def run(self):
         with HandLandmarker.create_from_options(self.options) as landmarker:
@@ -78,8 +77,6 @@ class HandDetector(QThread):
         x_position_index = index_landmark.x * screen_width
         y_position_index = (1 - index_landmark.y) * screen_height  # Flip y-coordinate
         distance = self.get_distance(x_position_thumb, y_position_thumb, x_position_index, y_position_index)
-        #print(distance)
-
         if not self.gesture_being_drawn and distance <= START_DRAW_THRESHOLD:
             print("Gesture started")
             self.gesture_being_drawn = True
@@ -93,8 +90,6 @@ class HandDetector(QThread):
                     self.gesture_being_drawn = False
                     self.over_threshold_counter = 0  # Reset counter after gesture ends
                     self.clear_scene_signal.emit()
-                    recognized_result = self.recognizer.Recognize([Point(p[0], p[1]) for p in self.line_points])
-                    self.gesture_recognized_signal.emit(recognized_result.Name, recognized_result.Score)
             else:
                 self.over_threshold_counter = 0  # Reset counter if distance is not over threshold
 
@@ -107,14 +102,12 @@ class HandDetector(QThread):
     def handle_result(self, result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
         hand_landmarks_list = result.hand_landmarks
 
-        if hand_landmarks_list is not None:
-            self.time_since_last_detection = time.time()
-
         if time.time() - self.time_since_last_detection > 1:
             self.clear_scene_signal.emit()
 
         # Loop through the detected hands to visualize.
         for idx in range(len(hand_landmarks_list)):
+            self.time_since_last_detection = time.time()
             hand_landmarks = hand_landmarks_list[idx]
             
             thumb_landmark = hand_landmarks[4] #https://ai.google.dev/edge/mediapipe/solutions/vision/hand_landmarker/index?hl=de#models
@@ -134,6 +127,10 @@ class LetterDrawer(QMainWindow):
         self.menu_opened = False
         self.dimensions = (screen_height, screen_width, 3) #set size to screen size to avoid mapping
         self.startLongRunning()
+
+         # Init Dollar Recognizer
+        self.recognizer = DollarRecognizer()
+        self.recognizer.load_unistrokes_from_xml()
 
     def initUI(self):
         self.setWindowTitle('Drawing Application')
@@ -162,7 +159,6 @@ class LetterDrawer(QMainWindow):
         self.hand_detector.clear_scene_signal.connect(self.clear_scene)
         self.hand_detector.add_line_point_signal.connect(self.try_add_line_point)
         self.hand_detector.draw_line_signal.connect(self.draw_line)
-        self.hand_detector.gesture_recognized_signal.connect(self.on_gesture_recognized)  # handle recognized gestures
         self.hand_detector.finished.connect(self.hand_detector.deleteLater)
         self.hand_detector.start()
 
@@ -179,7 +175,8 @@ class LetterDrawer(QMainWindow):
         if (abs(self.last_mouse_x - x_position_hand) < TOLERANCE and abs(self.last_mouse_y - y_position_hand) < TOLERANCE):
             if time.time() - self.time_since_last_point > 0.5:
                 if not self.menu_opened:
-                    self.open_command_menu("p", x_position_hand, y_position_hand)
+                    recognized_result = self.recognizer.Recognize([Point(p[0], p[1]) for p in self.line_points])
+                    self.on_gesture_recognized(recognized_result.Name, recognized_result.Score)
         # If the hand is moving, add the new point to the line
         else:
             self.menu_opened = False
@@ -202,11 +199,21 @@ class LetterDrawer(QMainWindow):
 
 
     def open_command_menu(self, detected_character, x, y):
-        print("Open command menu")
-        # commands = COMMANDS[detected_character]
-        # for i in range(len(commands)):
-        #     self.scene.addText(commands[i], self.pen).setPos(x, y + i * 20)
-        # self.menu_opened = True
+        print(detected_character)
+        #print(COMMANDS["commands"][detected_character])
+        #commands = COMMANDS["commands"][detected_character]
+        self.menu_opened = True
+        #for i in range(len(commands)): 
+        #    self.scene.addText(commands[i], QFont('Helvetica', 12)).setPos(x, y + i * 20)
+
+    def close_command_menu(self):
+        self.menu_opened = False
+        self.clear_scene()
+        self.draw_line()
+
+    def execute_command(self, command):
+        print("Command executed:", command)
+        # Add code to execute the command here
 
 
 app = QApplication(sys.argv)
--- documentation.md
+++ documentation.md
@@ -1,1 +1,11 @@
 # Documentation
+## Paper auswahl
+Die meisten Papers, die uns bei der Recherche begegneten waren innerhalb zwei Wochen nicht umsetzbar oder benötigten zu viel spezielle Technologien/Hardware.
+Neben dem Paper zu "Augmented Letters" kamen noch "EyeBookmark"- Ein mit hilfe von eye-tracking automatischer bookmarker von Text -, "DriftBoard"- Eine touch-basierte lösung für Tastaturen auf kleinen Geräten wie Smartwatches - und "LightAnchors"- Eine mit Lichtpunkten funktionierende räumliche verankerung von Daten.
+
+
+## Implementierung
+Wir entschieden uns, die Technick der "Augmented Letters" um eine Gesten-basierte Steuerung zu erweitern, die, mit hilfe von Handtracking, verschiedene Buchstaben erkennen, und befehle ausführen kann. Die Befehle können vom Nutzer individuell in einer "commands.json"-Datei eingestellt werden.
+
+### Graphische Darstellung
+Um dem Nutzer ein Visuelles Feedback seiner Eingabe zu geben wird über den gesamten Bildschirm ein unsichtbares Fenster gelegt. Auf diesem wird dann, sobald eine Geste erkannt wird, eine Linie gezeichnet. Sobald der Nutzer fertig mit Zeichnen ist, und sich nicht mehr bewegt, wird der gezeichnete Buchstabe analysiert, und entsprechend ein Menü mit den korrekten Befehlen geöffnet.
